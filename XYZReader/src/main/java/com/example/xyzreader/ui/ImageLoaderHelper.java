3cca44ec5391c47c657cbe9e84cb8f462f7fb868
Daniel W <dmachine777@gmail.com>
2019-09-08 19:40:55 -0500

    Initial Commit to working state. library dependencies are set up and app looks perfectly terrible. A great start.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ImageLoaderHelper.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ImageLoaderHelper.java
@@ -9,36 +9,34 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 public class ImageLoaderHelper {
-    private static ImageLoaderHelper sInstance;
+     private static ImageLoaderHelper sInstance;
+     private final LruCache<String, Bitmap> mImageCache = new LruCache<String, Bitmap>(20);
+     private ImageLoader mImageLoader;
+     private ImageLoaderHelper(Context applicationContext) {
+          RequestQueue queue = Volley.newRequestQueue(applicationContext);
+          ImageLoader.ImageCache imageCache = new ImageLoader.ImageCache() {
+               @Override
+               public void putBitmap(String key, Bitmap value) {
+                    mImageCache.put(key, value);
+               }
 
-    public static ImageLoaderHelper getInstance(Context context) {
-        if (sInstance == null) {
-            sInstance = new ImageLoaderHelper(context.getApplicationContext());
-        }
+               @Override
+               public Bitmap getBitmap(String key) {
+                    return mImageCache.get(key);
+               }
+          };
+          mImageLoader = new ImageLoader(queue, imageCache);
+     }
 
-        return sInstance;
-    }
+     public static ImageLoaderHelper getInstance(Context context) {
+          if (sInstance == null) {
+               sInstance = new ImageLoaderHelper(context.getApplicationContext());
+          }
 
-    private final LruCache<String, Bitmap> mImageCache = new LruCache<String, Bitmap>(20);
-    private ImageLoader mImageLoader;
+          return sInstance;
+     }
 
-    private ImageLoaderHelper(Context applicationContext) {
-        RequestQueue queue = Volley.newRequestQueue(applicationContext);
-        ImageLoader.ImageCache imageCache = new ImageLoader.ImageCache() {
-            @Override
-            public void putBitmap(String key, Bitmap value) {
-                mImageCache.put(key, value);
-            }
-
-            @Override
-            public Bitmap getBitmap(String key) {
-                return mImageCache.get(key);
-            }
-        };
-        mImageLoader = new ImageLoader(queue, imageCache);
-    }
-
-    public ImageLoader getImageLoader() {
-        return mImageLoader;
-    }
+     public ImageLoader getImageLoader() {
+          return mImageLoader;
+     }
 }
